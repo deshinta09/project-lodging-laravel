3459a049148a766b3977326ddd0cd9644cfc59fe
deshinta <deshintaaulia065@gmail.com>
2024-07-25 19:43:35 +0700

    feat: add read more in card lodging

--- cms/resources/views/lodging.blade.php
+++ cms/resources/views/lodging.blade.php
@@ -22,20 +22,9 @@
         <p class="text-slate-500 text-base">${{ $data['price'] }}</p>
     </div>
     <h5 class="text-slate-500 text-base pl-3">{{ $data->created_at->format('j F Y') }}</h5>
-    <h5 class="mt-4 pl-3">{{ $data['description'] }}</h5>
+    <h5 class="mt-4 pl-3">{{ Str::limit($data['description'], 50) }}</h5>
     <div class="flex justify-around p-3">
-      <a
-        class="rounded py-2 px-4 bg-sky-500 text-white hover:bg-sky-800"
-        href="#"
-      >
-        Detail
-      </a>
-      <a
-        href="#"
-        class="rounded py-2 px-4 border border-yellow-500 hover:text-white hover:bg-yellow-500"
-      >
-        Edit
-      </a>
+      <a href="/lodgings/{{ $data['id'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
     </div>
   </div>
 @endforeach
